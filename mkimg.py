9586856bb223219c0f147ef08ef9568401e2295f
ChronosNoob <83444922+ChronosNoob@users.noreply.github.com>
2023-11-16 22:30:17 +0000

    Add files via upload

--- mkimg.py
+++ mkimg.py
@@ -1,9 +1,28 @@
 import matplotlib
-from PIL import Image, ImageDraw, ImageFilter
+from PIL import Image, ImageDraw, ImageFilter, ImageFont
+import urllib.request
 
 def MakeImg(CompleteData):
     for subreddit in CompleteData:
         for post in subreddit:
             CurrentData = post 
-            print(CurrentData)
-            Img = Image.new(mode="RGB", size=(1920,1500))
+            Title = CurrentData[0]
+            BodyText = CurrentData[1]
+            Author = CurrentData[2]
+            imgurl = CurrentData[3]
+            Comments = CurrentData[4]
+            Canvas = Image.new(mode="RGB", size=(1920,1500), color=(50,50,50))
+            Draw = ImageDraw.Draw(Canvas) 
+            TitleFont = ImageFont.truetype(r'fonts/Roboto-Medium.ttf',162)
+            BodyFont = ImageFont.truetype(r'fonts/RedditSans-Regular.ttf',12)
+            Draw.text((30,15),Title,(255,255,255),Font=TitleFont)
+            if imgurl != False:
+                urllib.request.urlretrieve(imgurl,"img.png")
+                image = Image.open("img.png")
+                
+            Canvas.show()
+            Canvas.save("image.png")
+            break
+        break
+            
+            
